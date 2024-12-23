76b1098252a39845b5bb76d0a2c03290b3266527
Gattigorlavamshi <22bk1a7244@stpeters.com>
2024-12-23 12:19:01 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,30 @@
+class Emp{
+	int id;
+	String name;
+	int sal;
+	String address;
+	String company;
+	public Emp(int id, String name, int sal, String address, String company) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.sal = sal;
+		this.address = address;
+		this.company = company;
+	}
+	@Override
+	public String toString() {
+		return "Emp [id=" + id + ", name=" + name + ", sal=" + sal + ", address=" + address + ", company=" + company
+				+ "]";
+	}
+}
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Emp e1 = new Emp(723755,"vamsh+i",45000,"Hyd","Drl");
+		System.out.println(e1);
+
+	}
+
+}
